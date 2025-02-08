3e48adedbc96d4d25496b8474d7043f77c7217d2
David Fischer <fiscd@seas.upenn.edu>
2025-02-08 12:05:03 -0500

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Assessment Bias",
+  description: "Explore the impact of bias.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
