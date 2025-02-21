62f55020fae02d4b91f78b3bce3986dc67dd4f60
Jack Liddy <36009149+JackLiddy@users.noreply.github.com>
2025-02-21 11:24:52 -0500

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -8,6 +9,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* Google Analytics Script */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-BLD5JDPK5N"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-BLD5JDPK5N');
+              `,
+        }}
+      />
       <body>{children}</body>
     </html>
   );
